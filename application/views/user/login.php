3589fb2038d6d1ab4b9f13b0920c1915e5802f51
dwiaprikurniawan <dwiaprikrniawan@gmail.com>
2022-08-19 13:41:04 +0700

    tambahan menu fasilitas

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -12,7 +12,7 @@
   <!-- Password input -->
   <div class="form-outline mb-4">
     <input type="password" id="form2Example2" class="form-control" />
-    <label class="form-label" for="form2Example2">Passworddddd</label>
+    <label class="form-label" for="form2Example2">Password</label>
   </div>
 
   <!-- 2 column grid layout for inline styling -->
@@ -37,21 +37,6 @@
   <!-- Register buttons -->
   <div class="text-center">
     <p>Not a member? <a href="#!">Register</a></p>
-    <p>or sign up with:</p>
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-facebook-f"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-google"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-twitter"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-github"></i>
     </button>
   </div>
 </form>
